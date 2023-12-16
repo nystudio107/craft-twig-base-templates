13b5e09c5072d13128396675f32e8023fde1d8c5
Andrew Welch <andrew@nystudio107.com>
2023-12-16 18:45:25 -0500

    feat: Include components with attributes

---
+++ src/attributes/PrefectUrls.php
@@ -0,0 +1,17 @@
+<?php
+namespace nystudio107\craftTwigBaseTemplates\attributes;
+
+use Attribute;
+
+#[Attribute]
+class PrefectUrls
+{
+    /* @var string[] $prefetchUrls The URLs to prefetch */
+    public array $prefetchUrls = [];
+
+    /* @var bool $outputLinks Whether <link> tags should be output */
+    public bool $outputLinks = true;
+
+    /* @var bool $outputHeaders Whether Link: headers should be output */
+    public bool $outputHeaders = true;
+}
